5c85977fd5ec7e59acff7bb55956fce23ea290f4
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-01-03 21:48:15 +0900

    updated

---
+++ Lesson/Lesson.playground/Contents.swift
@@ -0,0 +1,95 @@
+import UIKit
+
+/* Hello! World.を表示 */
+func Lesson_HelloWorld() {
+   print("Hello! World.") // Hello! World.
+}
+print("---- Lesson_HelloWorld ----")
+Lesson_HelloWorld()
+print("------------------\n")
+
+/* 変数と定数 */
+func Lesson_LetConst() {
+    var count: Int = 1
+    count += 1
+    
+    var message: String = ""
+    message = "Lesson \(count)"
+    print(message)  // Lesson 2
+    
+    // 定数
+    let name: String = "山田太郎"
+    print(name)     // 山田太郎
+}
+print("---- Lesson_LetConst ----")
+Lesson_LetConst()
+print("------------------\n")
+
+/* プリミティブ型、Any型  */
+func Lesson_PriAny() {
+    // プリミティブ型
+    let numInt: Int = 10            // Int型（整数のみ）
+                                    // 32bitの場合 2147483647 〜 -2147483648
+                                    // 64bitの場合 -9223372036854775808 〜　9223372036854775807
+    let numUint: UInt = 100         // UInt型（正の整数のみ）
+                                    // 32bitの場合 0 〜 -2147483648
+                                    // 64bitの場合 0 〜　9223372036854775807
+    let numDouble: Double = 10.123  // Double型（浮動小数点数型） 64bit
+    let numFloat: Float = 10.123    // Double型（浮動小数点数型） 32bit
+    let str: String = "文字列"       // String型
+    let isActive: Bool = true       // Bool型（trueかfalse）
+    
+    print("numInt \(numInt)")       // numInt 10
+    print("numUint \(numUint)")     // numUint 100
+    print("numDouble \(numDouble)") // numDouble 10.123
+    print("numFloat \(numFloat)")   // numFloat 10.123
+    print("str \(str)")             // str 文字列
+    print("isActive \(isActive)")   // isActive true
+    
+    // Any型（なんでも入る）
+    var variable: Any
+    variable = 1
+    print("number \(variable)") // number 1
+    
+    variable = "文字列"
+    print("String \(variable)") // String 文字列
+    
+    variable = false
+    print("Bool \(variable)")   // Bool false
+    
+    variable = Date()
+    print("class \(variable)")  // class 2019-01-03T00:23:46.481Z
+}
+print("---- Lesson_PriAny ----")
+Lesson_PriAny()
+print("------------------\n")
+
+/* Optional型 */
+/* null安全 */
+/* Optional Binding */
+/* 演算子 */
+/* Dictionary型 */
+/* 配列 */
+/* if分 */
+/* 比較演算子 */
+/* AND・OR */
+/* 三項演算子 */
+/* switch文 */
+/* while文 */
+/* for文 */
+/* 関数 */
+/* Enum */
+/* 構造体 */
+/* クラス */
+/* 値渡し参照渡し */
+/* キャスト変換 */
+/* 日付関数（Date） */
+/* 数値計算（Math） */
+/* static */
+/* 継承 */
+/* プロトコル */
+/* 拡張関数 */
+/* ジェネリクス */
+/* クロージャ */
+/* コールバック*/
+/* ReactiveX */
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Swift-Training
+
+> 初学者向け Swift構文理解のためのソースコード
+## 開発環境
+- Xcode 10.1以上
+- Swift 4.0以上
+
+## 手順
+Xcodeのplaygounrdを利用してコーディングします。
+
+### 環境構築
+以下の手順を実行して
+1. Xcodeを起動
+2. Get started with playgoundを選択
+3. Blankを洗濯してNextボタン押下、playgroundの保存先を指定して保存
+
+### Hello! world.を表示する
+さきほど保存したplaygoundで以下のように実装してください。
+```swift
+import UIKit
+
+/* Hello! World.を表示 */
+print("Hello! World.")
+```
+エディタ下部の「▷」ボタンを押下すると実行します。<br>
+エディタツールバー -> Editer -> Run Playgroundでも実行できます。<br>
+<br>
+実行するとエディタ右側と下部の画面に "Hello world!"が表示されます。<br>
+<br>
+以上です。<br>
+以降、随時コンテンツを追加していきます。
