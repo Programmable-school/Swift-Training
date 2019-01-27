73063c862e9385b3c01da7e196720e7a66f9b7a4
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-01-27 15:04:45 +0900

    演算子

--- Lesson/Lesson.playground/Contents.swift
+++ Lesson/Lesson.playground/Contents.swift
@@ -131,6 +131,30 @@ lessonOptionalBinding()
 print("------------------\n")
 
 /* 演算子 */
+func lessonCalc() {
+    let variable: Int = 1
+    let resultA: Int = variable + 2
+    let resultB: Int = variable * 2
+    let resultC: Int = variable / 2
+    print(resultA)      // 3
+    print(resultB)      // 2
+    print(resultC)      // 0
+    
+    var resultD: Int = 1
+    resultD += 2
+    var resultE: Int = 1
+    resultE *= 2
+    var resultF: Int = 1
+    resultF /= 2
+    print(resultD)      // 3
+    print(resultE)      // 2
+    print(resultF)      // 0
+}
+/* 演算子 */
+print("---- Lesson_Calc ----")
+lessonCalc()
+print("------------------\n")
+
 /* 配列 */
 /* Dictionary型 */
 /* filter, map, reduce, sort */
