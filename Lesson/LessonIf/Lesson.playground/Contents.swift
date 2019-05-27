9a6e92c9faad7f9c52d504805732f933909f36dc
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-05-27 10:15:04 +0900

    Update Contents.swift

--- Lesson/LessonIf/Lesson.playground/Contents.swift
+++ Lesson/LessonIf/Lesson.playground/Contents.swift
@@ -19,7 +19,7 @@ if (value == 1) {
 if (value == 1) {
     print("valueは1です。")     // ここは通らない
 } else if (value == 2) {
-    print("valueは2です。")     // ここを通る/Users/hukusuke/Desktop/Start/Development/mokumoku/swift/Swift-Training/Lesson/LessonCompare/Lesson.playground/Contents.swift
+    print("valueは2です。")     // ここを通る
 } else {
     print("valueは1以外です。")   // ここは通らない
 }
